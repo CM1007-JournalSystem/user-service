a4ec9e9f86b9a49ccda8e3df019da49354f7c98d
William Ramberg <117517479+WillRamberg@users.noreply.github.com>
2025-01-28 21:53:33 +0100

    Fixed keycloak

--- src/main/java/kth/userservice/repository/UserRepository.java
+++ src/main/java/kth/userservice/repository/UserRepository.java
@@ -18,6 +18,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findAll();
 
-    Optional<User> getUserBykeycloak_id(UUID id);
+    Optional<User> getUserBykeycloakId(UUID id);
 
 }
